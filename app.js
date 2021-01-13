a646b0c2aa3895cf2678d372a5cda95a3bf2fb54
ismailonur <ismailonur15@gmail.com>
2021-01-13 20:26:28 +0300

    data yapısı oluşturuldu ve app modülü içerinden çalıştırıldı

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
 // Storage Controller
-const StorageController =(function(){
+const StorageController = (function () {
 
 
 })();
 
 // Product Controller
-const ProductController = (function(){
+const ProductController = (function () {
 
     // Private
+    const Product = function (id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    const data = {
+        products: [
+            { id: 0, name: "Laptop", price: 8000 },
+            { id: 0, name: "İşlemci", price: 4000 },
+            { id: 0, name: "Ram", price: 2000 }
+        ],
+        selectedProduct: null,
+        totalPrice: 0,
+    }
 
     // Public
-    return{
-        
+    return {
+        getProducts: function(){
+            return data.products;
+        },
+        getData: function(){
+            return data;
+        }
     }
 })();
 
 // UI Controller
-const UIController = (function(){
+const UIController = (function () {
 
 
 })()
 
 // App Controller
-const AppController = (function(ProductCtrl, UICtrl){
-
+const App = (function (ProductCtrl, UICtrl) {
+    return{
+        init: function(){
+            console.log("Uygulama Çalışıyor!");
+            const products = ProductCtrl.getProducts();
+            console.log(products);
+        }
+    }
 
 })(ProductController, UIController);
+
+App.init();
